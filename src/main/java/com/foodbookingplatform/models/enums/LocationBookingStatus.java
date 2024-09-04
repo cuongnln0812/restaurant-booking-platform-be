d102e45268d20ad6a036048edd735781b6c7e0f3
Son-2003 <113695548+Son-2003@users.noreply.github.com>
2024-09-04 07:42:11 +0700

    modify type Enum

--- src/main/java/com/foodbookingplatform/models/enums/LocationBookingStatus.java
+++ src/main/java/com/foodbookingplatform/models/enums/LocationBookingStatus.java
@@ -1,18 +1,9 @@
 package com.foodbookingplatform.models.enums;
 
 public enum LocationBookingStatus {
-    FAILED(0),
-    PENDING(1),
-    CONFIRMED(2),
-    CANCELLED(3),
-    SUCCESSFUL(4);
-    private final int value;
-
-    LocationBookingStatus(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    FAILED,
+    PENDING,
+    CONFIRMED,
+    CANCELLED,
+    SUCCESSFUL
 }

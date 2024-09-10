b10b556388fd7a6531375875ca0980252a318d10
NTDung93 <nguyentiendung2003@gmail.com>
2024-09-10 23:39:44 +0700

    add crud features for entity Category

--- src/main/java/com/foodbookingplatform/controllers/CategoryController.java
+++ src/main/java/com/foodbookingplatform/controllers/CategoryController.java
@@ -1,10 +1,9 @@
 package com.foodbookingplatform.controllers;
 
 import com.foodbookingplatform.models.constants.AppConstants;
-import com.foodbookingplatform.models.payload.dto.brand.BrandRequest;
-import com.foodbookingplatform.models.payload.dto.brand.BrandResponse;
-import com.foodbookingplatform.services.BrandService;
-import jakarta.validation.Valid;
+import com.foodbookingplatform.models.payload.dto.category.CategoryRequest;
+import com.foodbookingplatform.models.payload.dto.category.CategoryResponse;
+import com.foodbookingplatform.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,53 +12,51 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/brands")
+@RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
-public class BrandController {
-    private final BrandService brandService;
+public class CategoryController {
+    private final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<BrandResponse>> getAllBrands(
+    public ResponseEntity<List<CategoryResponse>> getAllCategories(
             @RequestParam(name = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(name = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
             @RequestParam(name = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
             @RequestParam(name = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ) {
-        return ResponseEntity.ok(brandService.getAllBrands(pageNo, pageSize, sortBy, sortDir));
+        return ResponseEntity.ok(categoryService.getAllCategories(pageNo, pageSize, sortBy, sortDir));
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<BrandResponse>> searchBrands(
+    public ResponseEntity<List<CategoryResponse>> searchCategories(
             @RequestParam(name = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(name = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
             @RequestParam(name = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
             @RequestParam(name = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir,
             @RequestParam(name = "keyword") String keyword
     ) {
-        return ResponseEntity.ok(brandService.searchBrands(pageNo, pageSize, sortBy, sortDir, keyword));
+        return ResponseEntity.ok(categoryService.searchCategories(pageNo, pageSize, sortBy, sortDir, keyword));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<BrandResponse> getBrand(@PathVariable Long id) {
-        return ResponseEntity.ok(brandService.getBrand(id));
+    public ResponseEntity<CategoryResponse> getCategory(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.getCategory(id));
     }
 
     @PostMapping
-    public ResponseEntity<BrandResponse> addBrand(@RequestBody @Valid BrandRequest request) {
-        BrandResponse response = brandService.addBrand(request);
+    public ResponseEntity<CategoryResponse> addCategory(@RequestBody CategoryRequest request) {
+        CategoryResponse response = categoryService.addCategory(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
-//        return new ResponseEntity<>(brandService.addBrand(request), HttpStatus.CREATED);
     }
 
     @PutMapping
-    public ResponseEntity<BrandResponse> updateBrand(@RequestBody @Valid BrandRequest request) {
-        return ResponseEntity.ok(brandService.updateBrand(request));
+    public ResponseEntity<CategoryResponse> updateCategory(@RequestBody CategoryRequest request) {
+        return ResponseEntity.ok(categoryService.updateCategory(request));
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteBrand(@PathVariable Long id) {
-        brandService.deleteBrand(id);
-        return ResponseEntity.ok("Brand deleted successfully");
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.ok("Category deleted successfully");
     }
 }
-

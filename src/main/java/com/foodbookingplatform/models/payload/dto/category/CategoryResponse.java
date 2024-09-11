739a309333029579f3aca0228a137bcd72377a21
NTDung93 <nguyentiendung2003@gmail.com>
2024-09-11 14:34:40 +0700

    update category field

--- src/main/java/com/foodbookingplatform/models/payload/dto/category/CategoryResponse.java
+++ src/main/java/com/foodbookingplatform/models/payload/dto/category/CategoryResponse.java
@@ -1,21 +1,12 @@
 package com.foodbookingplatform.models.payload.dto.category;
 
-import com.foodbookingplatform.models.entities.Location;
 import com.foodbookingplatform.models.enums.EntityStatus;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -25,7 +16,7 @@ public class CategoryResponse {
     private Long id;
     private String image;
     private String status;
-    private EntityStatus categoryName;
+    private EntityStatus name;
     private String createdBy;
     private LocalDateTime createdDate;
     private String modifiedBy;
